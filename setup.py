be4cd696dc0062da3355cdbcc625af30758dcef4
doi <126680758+nudoi@users.noreply.github.com>
2024-12-01 18:40:05 +0900

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyfieldimager',
-    version='0.9.2',
+    version='0.9.2.1',
     license='LGPLv2',
     description='A Python package for field image analysis.',
 
